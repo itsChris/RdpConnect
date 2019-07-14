77eea89abd2b21328c60fea569b1af6f7d5035a5
Péter Sturm <sturm.peti@gmail.com>
2019-07-14 19:23:02 +0200

    Added factory to handle RemoteDesktop creation

---
+++ RdpConnect.Core/Factories/RemoteDesktopFactory.cs
@@ -0,0 +1,27 @@
+﻿using RdpConnect.Core.Entities;
+using System.Collections.Generic;
+
+namespace RdpConnect.Core.Factories
+{
+    static public class RemoteDesktopFactory
+    {
+        static public RemoteDesktop CreateRemoteDesktop(Client client, string IP, int port)
+        {
+            var remotedesktop = new RemoteDesktop(IP, port);
+            client.AddRemoteDestop(remotedesktop);
+
+            return remotedesktop;
+        }
+
+        static public RemoteDesktop CreateRemoteDesktopWidthRemoteUsers(Client client, string IP, int port, IList<RemoteUser> remoteUsers)
+        {
+            var remotedesktop = new RemoteDesktop(IP, port);
+            foreach (var remoteuser in remoteUsers)
+                remotedesktop.AddRemoteUser(remoteuser);
+
+            client.AddRemoteDestop(remotedesktop);
+
+            return remotedesktop;
+        }
+    }
+}
